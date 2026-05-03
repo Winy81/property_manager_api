74b27d9e7db9986b3f8323d149b62507c1b36c99
Admin <admin@Admins-MacBook-Pro.local>
2026-05-03 19:41:31 +0100

    Versioning the exist controller actions (push actual changes)

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,12 +1,17 @@
-class ServicesController < ApplicationController
+module Api
+  module V1
+    class ServicesController < ApplicationController
 
-  def index
-    @services = Service.all
-    render json: @services
-  end
+      def index
+        @services = Service.all
+        render json: @services
+      end
 
-  def show
-    @service = Service.find(params[:id])
-    render json: @service
+      def show
+        @service = Service.find(params[:id])
+        render json: @service
+      end
+      
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :services, only: [:index, :show]
+
+  namespace :api do
+    namespace :v1 do
+      resources :services, only: [:index, :show]
+    end
+  end
+  
 end
